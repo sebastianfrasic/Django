2a6b5af0046339e86514ab0aefbb251302310e8f
unknown <2145120@labinfo.is.escuelaing.edu.co>
2019-10-07 14:08:49 -0500

    Entrar a la pagina de administración 127.0.0.1:8000/admin

--- PrimeraSesion/PreguntasRespuestas/Preguntas/models.py
+++ PrimeraSesion/PreguntasRespuestas/Preguntas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Pregunta(models.Model):
+    fechaPublicacion = models.DateTimeField('Fecha publicacion')
+    textoPregunta = models.CharField(max_length=200)
+    def __str__(self):
+        return self.textoPregunta
+
+
+class Respuesta(models.Model):
+    preguntaAsociada = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    textoRespuesta = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.textoRespuesta
